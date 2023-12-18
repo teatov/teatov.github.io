644052ed94b2ed8559f49409d604e5db42873d70
teatov <hlebpoimenigleb@gmail.com>
2023-12-18 23:26:46 +1000

    fix icons

--- src/icons/tabler-icons.ts
+++ src/icons/tabler-icons.ts
@@ -1,3 +1,16 @@
-import { createIconPack } from "astro-icon/pack";
+import { icons } from '@iconify-json/tabler';
+import { getIconData, iconToSVG, iconToHTML, replaceIDs } from '@iconify/utils';
 
-export default createIconPack({ package: "@tabler/icons", dir: "icons" });
+export default (name: string) => {
+  const iconData = getIconData(icons, name);
+
+  if (!iconData) {
+    throw new Error(`Icon "${name}" is missing`);
+  }
+
+  const renderData = iconToSVG(iconData, {});
+
+  const svg = iconToHTML(replaceIDs(renderData.body), renderData.attributes);
+
+  return svg;
+};
