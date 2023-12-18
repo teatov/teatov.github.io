065d92390c4165a5039ef7c9e37904746d02305b
teatov <hlebpoimenigleb@gmail.com>
2023-12-19 04:52:30 +1000

    c

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,25 +5,15 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     colors: {
-      twhite: '#F4F1DE',
-      tred: '#E07A5F',
-      tpurple: '#1b1d2e',
-      tgreen: '#81B29A',
-      tyellow: '#F2CC8F',
+      white: '#F4F1DE',
+      red: '#E07A5F',
+      purple: '#1b1d2e',
+      green: '#81B29A',
+      yellow: '#F2CC8F',
     },
     fontFamily: {
       sans: ['Ubuntu', ...fontFamily.sans],
       mono: ['Ubuntu Mono', ...fontFamily.mono],
-    },
-    extend: {
-      animation: {
-        shine: 'shine 1s',
-      },
-      keyframes: {
-        shine: {
-          '100%': { left: '125%' },
-        },
-      },
     },
   },
 } satisfies Config;
