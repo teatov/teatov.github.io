3ae44335a4fe3a545535b1bf4151d6892a22a233
teatov <hlebpoimenigleb@gmail.com>
2024-03-25 03:45:45 +1000

    update dependencies and workflow

--- astro.config.ts
+++ astro.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://teatov.github.io',
-  base: '/',
   integrations: [
     tailwind({
       applyBaseStyles: false,
