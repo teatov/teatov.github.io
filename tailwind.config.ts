916b19cdaa2154803891697802afc77ace1845f1
teatov <hlebpoimenigleb@gmail.com>
2024-03-25 04:46:55 +1000

    add optimizeUniversalDefaults

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,9 @@ import { fontFamily } from 'tailwindcss/defaultTheme';
 
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  experimental: {
+    optimizeUniversalDefaults: true,
+  },
   theme: {
     colors: {
       black: '#000000',
