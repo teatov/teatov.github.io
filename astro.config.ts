49228cdc8a44058772115837dce3053dd2564f20
Teatov <hlebpoimenigleb@gmail.com>
2025-04-16 02:43:38 +1000

    change email text and add links

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://teatov.github.io',
+  site: 'https://teatov.xyz',
   integrations: [
     tailwind({
       applyBaseStyles: false,
