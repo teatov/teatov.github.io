e4261a11a4e0144d24557970ee651a8fdca5ee5c
teatov <hlebpoimenigleb@gmail.com>
2025-09-07 00:10:41 +1000

    add sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,19 @@
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://teatov.xyz',
+  trailingSlash: 'never',
+  build: {
+    assets: '_assets',
+  },
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
+    sitemap(),
   ],
   markdown: {
     syntaxHighlight: 'prism',
